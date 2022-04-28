e9bb29fa58f1c7104a2379cdf2705c5cbf4340e5
Rogerio Chaves Barreto <rogerio.barreto@ssp.ba.gov.br>
2022-04-27 23:04:37 -0300

    feat: done challange 1

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,44 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const updatedUser = Object.assign(receivedUser, {
+      updated_at: new Date(),
+      admin: true,
+    });
+
+    this.users.map((user) => {
+      if (user.id === receivedUser.id) return updatedUser;
+      return user;
+    });
+
+    return updatedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
